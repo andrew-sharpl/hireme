29dd006a424be8c8f5f8b2763a99c8b2c89b8cdd
andrew-sharpl <jasharpluevano@gmail.com>
2026-03-30 18:59:40 -0400

    Configure EF Core with MySQL database connection

--- HireMe.API/Program.cs
+++ HireMe.API/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using HireMe.API.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Registers DbContext to connect to MySQL database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
